92d108e1bd01e307068e59991a4cfa11643c3690
Gabriel Chow <gabrielchow93@gmail.com>
2013-04-29 14:23:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpringlabTest::Application.config.secret_token = 'bbd6b66c7fe9a777df5d0c7e9aaa7d0352bd58bbc5158fdb733ac436051d4227fa885aa3ee48e188c29606d623739785410f977877ea8707896ff02fbbf87a4f'
